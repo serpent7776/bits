0b66e97e4568abdb596f5bbf871d6372e42e7298
Serpent7776 <Serpent7776@gmail.com>
2022-12-10 14:13:13 +0100

    Add tests and benchmarks

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
-CXXFLAGS=-std=c++20 -Wall -Wextra -pedantic -O2
+CXXFLAGS=-std=c++20 -Wall -Wextra -pedantic -O2 -ggdb3
 
+.PHONY: test
 test: str_join
 	./str_join
 
 str_join: main.o str_join.o
-	g++ -o str_join  main.o str_join.o
+	${CXX} ${CXXFLAGS} -o str_join  main.o str_join.o
--- str_join.cpp
+++ str_join.cpp
@@ -1,29 +1,102 @@
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
 #include "catch.hpp"
 
-#include <iostream>
 #include <string>
 #include <string_view>
 
-template<typename Sep, typename String, typename ...Strings>
-std::string str_join(Sep&& sep, String&& string, Strings&&... strings)
+std::string str_join_v([[ maybe_unused ]]std::string_view sep, auto... strings)
 {
-	std::string buf = string;
-	(([&buf, sep](auto&& str){
-		if (!buf.empty() && !std::string_view(str).empty())
+	std::string buf;
+	([&](std::string_view str){
+		if (!str.empty())
 		{
-			buf += sep;
+			(buf += str) += sep;
 		}
-		buf += str;
-	}(std::forward<Strings>(strings))), ...);
+	}(strings), ...);
+	if (!buf.empty())
+	{
+		buf.pop_back();
+	}
 	return buf;
 }
-
-TEST_CASE("tests")
+std::string str_join(std::string_view sep, const auto&... strings)
 {
-	REQUIRE(str_join('x', "test") == "test");
-	REQUIRE(str_join(' ', "hello", "world") == "hello world");
-	REQUIRE(str_join('-', "foo", "bar", "baz") == "foo-bar-baz");
-	REQUIRE(str_join('*', "a", "b", "c", "d") == "a*b*c*d");
-	REQUIRE(str_join(';', "", "", "", "x", "y") == "x;y");
-	REQUIRE(str_join(';', "x", "", "", "x") == "x;x");
+	return str_join_v(sep, std::string_view(strings)...);
+}
+std::string str_join(char sep, const auto&... strings)
+{
+	std::string_view sep_view(&sep, 1);
+	return str_join_v(sep_view, std::string_view(strings)...);
+}
+
+const char* string_literal(const char* s)
+{
+	return s;
+}
+
+std::string string(const char* s)
+{
+	return s;
+}
+
+std::string_view string_view(const char* s)
+{
+	return s;
+}
+
+TEST_CASE("string literal tests")
+{
+	auto f = string_literal;
+	CHECK(str_join('x', f("test"), "") == "test");
+	CHECK(str_join(' ', f("hello"), f("world")) == "hello world");
+	CHECK(str_join('-', f("foo"), f("bar"), f("baz")) == "foo-bar-baz");
+	CHECK(str_join('*', f("a"), f("b"), f("c"), f("d")) == "a*b*c*d");
+	CHECK(str_join(';', f(""), f(""), f(""), f("x"), f("y")) == "x;y");
+	CHECK(str_join(';', f("x"), f(""), f(""), f("x")) == "x;x");
+	CHECK(str_join(';', f("")) == "");
+}
+
+TEST_CASE("string tests")
+{
+	auto f = string;
+	CHECK(str_join('x', f("test")) == "test");
+	CHECK(str_join(' ', f("hello"), f("world")) == "hello world");
+	CHECK(str_join('-', f("foo"), f("bar"), f("baz")) == "foo-bar-baz");
+	CHECK(str_join('*', f("a"), f("b"), f("c"), f("d")) == "a*b*c*d");
+	CHECK(str_join(';', f(""), f(""), f(""), f("x"), f("y")) == "x;y");
+	CHECK(str_join(';', f("x"), f(""), f(""), f("x")) == "x;x");
+	CHECK(str_join(';', f("")) == "");
+}
+
+TEST_CASE("string view tests")
+{
+	auto f = string_view;
+	CHECK(str_join('x', ("test")) == "test");
+	CHECK(str_join(' ', f("hello"), f("world")) == "hello world");
+	CHECK(str_join('-', f("foo"), f("bar"), f("baz")) == "foo-bar-baz");
+	CHECK(str_join('*', f("a"), f("b"), f("c"), f("d")) == "a*b*c*d");
+	CHECK(str_join(';', f(""), f(""), f(""), f("x"), f("y")) == "x;y");
+	CHECK(str_join(';', f("x"), f(""), f(""), f("x")) == "x;x");
+	CHECK(str_join(';', f("")) == "");
+}
+
+TEST_CASE("benchmark")
+{
+	std::string s;
+	BENCHMARK("str_join 1")
+	{
+		s = str_join(".", "a");
+	};
+	BENCHMARK("str_join 2")
+	{
+		s = str_join(".", "a", "b");
+	};
+	BENCHMARK("str_join 3")
+	{
+		s = str_join(".", "a", "b", "c");
+	};
+	BENCHMARK("str_join 4")
+	{
+		s = str_join(".", "a", "b", "c", "d");
+	};
 }
