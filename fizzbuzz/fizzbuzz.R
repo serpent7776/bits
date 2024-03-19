306663d6a2bd14f971058a0a64ea8b6057ca2ef3
Serpent7776 <Serpent7776@gmail.com>
2024-03-19 22:34:13 +0100

    Save few bytes in fizzbuzz in R

--- fizzbuzz/fizzbuzz.R
+++ fizzbuzz/fizzbuzz.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 c <- 1:100
-c[seq(0, 100, 3)[-1]] <- "fizz"
-c[seq(0, 100, 5)[-1]] <- "buzz"
-c[seq(0, 100, 15)[-1]] <- "fizzbuzz"
+c[seq(0, 100, 3)] <- "fizz"
+c[seq(0, 100, 5)] <- "buzz"
+c[seq(0, 100, 15)] <- "fizzbuzz"
 cat(paste(c, collapse="\n"), sep="\n")
