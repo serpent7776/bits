dd02ef936eb1a8afb3c5b12dd2dac59f2833248d
Serpent7776 <Serpent7776@gmail.com>
2024-03-27 11:50:08 +0100

    Shorter perl fizzbuzz

--- fizzbuzz/fizzbuzz.pl
+++ fizzbuzz/fizzbuzz.pl
@@ -3,4 +3,4 @@ use strict;
 use warnings;
 use feature 'say';
 
-say for map {('', 'fizz')[$_%3==0].('', 'buzz')[$_%5==0] || $_} 1..100
+say for map {'fizz' x ($_%3==0) . 'buzz' x ($_%5==0) || $_} 1..100
