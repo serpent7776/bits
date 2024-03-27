9b54d400dc1b13c7ef83d58fbb8c4f99832ecdc1
Serpent7776 <Serpent7776@gmail.com>
2024-03-27 19:45:19 +0100

    Shorter fizzbuzz in julia

--- fizzbuzz/fizzbuzz.jl
+++ fizzbuzz/fizzbuzz.jl
@@ -1,4 +1,3 @@
 #!/usr/bin/env julia
 
-a = map(n -> [string(n), "fizz", "buzz", "fizzbuzz"][1+((n % 3 == 0) + 2 * (n % 5 == 0))], 1:100)
-println(join(a, "\n"))
+println(join((n -> join([n, "fizz", "buzz"][[n%3>0 && n%5>0, n%3==0, n%5==0]])).(1:100), "\n"))
