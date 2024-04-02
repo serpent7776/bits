9287895d3f87702b2ecd6d8d5006756caff2de58
Serpent7776 <Serpent7776@gmail.com>
2024-04-02 18:57:04 +0200

    Fizzbuzz in sql

---
+++ fizzbuzz/fizzbuzz.sql
@@ -0,0 +1,8 @@
+WITH RECURSIVE
+   cnt(x) AS (
+      SELECT 1
+      UNION ALL
+      SELECT x+1 FROM cnt
+       where x < 100
+   ) select coalesce(nullif(case when n.x%3=0 then 'fizz' else '' end || case when n.x%5=0 then 'buzz' else '' end, ''), CAST(n.x AS text)) from cnt as n;
+
