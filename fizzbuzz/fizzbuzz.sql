8ed637cd481a87049374cab7cad5c0fbf2860de4
Serpent7776 <Serpent7776@gmail.com>
2024-04-06 22:59:56 +0200

    Reformat fizzbuzz in sql

--- fizzbuzz/fizzbuzz.sql
+++ fizzbuzz/fizzbuzz.sql
@@ -1,8 +1,13 @@
 WITH RECURSIVE
-   cnt(x) AS (
-      SELECT 1
-      UNION ALL
-      SELECT x+1 FROM cnt
-       where x < 100
-   ) select coalesce(nullif(case when n.x%3=0 then 'fizz' else '' end || case when n.x%5=0 then 'buzz' else '' end, ''), CAST(n.x AS text)) from cnt as n;
+cnt(x) AS (
+	SELECT 1
+	UNION ALL
+	SELECT x+1 FROM cnt
+	where x < 100
+) select coalesce(nullif(
+		case when n.x%3=0 then 'fizz' else '' end ||
+		case when n.x%5=0 then 'buzz' else '' end,
+		''),
+	CAST(n.x AS text))
+from cnt as n;
 
