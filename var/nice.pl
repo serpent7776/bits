a4f2ddc5e035bed188eeb62e457f5fb0310f6a7c
Serpent7776 <Serpent7776@gmail.com>
2024-04-09 21:59:54 +0200

    More obfuscated nice.pl

--- var/nice.pl
+++ var/nice.pl
@@ -2,7 +2,6 @@
 use strict;
 use warnings;
 
-my@a=sub{my@a=@_;
-map{(-10, -12, 86, 56, -82, -5, -82, 1, 73, 54, -13, -70)[$_]+$a[$_]} 0..11
-}->((unpack("C*", 'rm -rf')) x 2);my@w=(0x64, 0x61, 0x79);
-print eval "'" . pack("C*", @a) . pack("C*", @w) . "\n'";
+print eval "'" . pack("C*", sub{
+map{(-10, -12, 86, 56, -82, -5, -82, 1, 73, 54, -13, -70)[$_]+$_[$_]} 0..11
+}->((unpack("C*", 'rm -rf')) x 2)) . pack("C*", (0x64, 0x61, 0x79)) . "\n'";
