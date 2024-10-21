05d7223bf938350cdad95a710fa2b2382017d22c
Serpent7776 <Serpent7776@gmail.com>
2024-10-21 20:30:48 +0200

    Some linter fixes

--- day/day.R
+++ day/day.R
@@ -4,7 +4,7 @@ y <- as.integer(a[1])
 m <- as.integer(a[2])
 d <- as.integer(a[3])
 
-M <- c( 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 )
+M <- c(0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
 ry <- 1970
 rm <- 1
@@ -15,5 +15,5 @@ is_leap <- function(y) (y %% 4 == 0 && y %% 100 != 0) || y %% 400 == 0
 leaps <- sum(sapply(1970:y, is_leap))
 dd <- (d-1) + sum((M)[1:m]) + (y-ry) * 365 + leaps + (-1 * (is_leap(y) && m %in% c(1,2)))
 
-days <- c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 print(days[1 + (dd + 7 - rn) %% 7])
