a1b45e75dc0af00c966a02975dc2d1a419d6082e
Serpent7776 <Serpent7776@gmail.com>
2024-11-24 19:48:27 +0100

    Sorry in BQN and K

--- sorry/sorry.sh
+++ sorry/sorry.sh
@@ -7,9 +7,13 @@ sh -c 'for i in `seq 1000`; do echo Sorry; done'
 lua -e 'print(string.rep("Sorry\n", 1000))'
 sh -c 'printf "%0.sSorry\n" $(seq 1000)'
 perl -e 'print "Sorry\n"x1000'
+bqn -e '•Out¨1000/⟨"Sorry"⟩'
 R -q -s --no-save <<eof
 cat(strrep("Sorry\n", 1000))
 eof
 tclsh <<eof
 puts [string repeat "Sorry\n" 1000]
+eof
+ngnk <<eof
+`0:1000#,$`Sorry
 eof
